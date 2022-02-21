9aec62e7b0551e43dd354c2222edd2e4cf463061
Devin Adams <68407562+insultfromafar@users.noreply.github.com>
2022-02-21 07:46:23 -0800

    Swapped back to 3D camera

--- go.go
+++ go.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/gen2brain/raylib-go/raygui"
 	"github.com/gen2brain/raylib-go/raylib"
 	hex "github.com/hautenessa/hexagolang"
 )
@@ -21,14 +20,14 @@ type window struct {
 }
 
 const (
-	borderwidth  = int32(50)
-	panSpeed     = .5
+	borderwidth  = int32(100)
+	panSpeed     = .75
 	screenWidth  = int32(1600)
 	screenHeight = int32(900)
 )
 
 func main() {
-	rl.SetConfigFlags(rl.FlagFullscreenMode)
+	//rl.SetConfigFlags(rl.FlagFullscreenMode)
 	rl.InitWindow(screenWidth, screenHeight, "Window")
 
 	camera := rl.Camera{
@@ -37,13 +36,6 @@ func main() {
 		Up:         rl.NewVector3(0.0, 1.0, 0.0),
 		Fovy:       75,
 		Projection: rl.CameraOrthographic,
-	}
-
-	secondCamera := rl.Camera2D{
-		Offset:   rl.NewVector2(0, 0),
-		Target:   rl.NewVector2(0, 0),
-		Rotation: 0.0,
-		Zoom:     1.0,
 	}
 
 	screenOrigin := hex.F{X: 0, Y: 0}
@@ -64,25 +56,24 @@ func main() {
 		E: float32(screenWidth),
 	}
 
-	//	top := rl.Rectangle{X: screen.W + float32(borderwidth), Y: screen.N, Width: screen.E - (float32(borderwidth) * 2), Height: float32(borderwidth)}
-	//	bottom := rl.Rectangle{X: screen.W + float32(borderwidth), Y: screen.S - float32(borderwidth), Width: screen.E - (float32(borderwidth) * 2), Height: float32(borderwidth)}
-	//	left := rl.Rectangle{X: screen.W, Y: screen.N + float32(borderwidth), Width: float32(borderwidth), Height: screen.S - (float32(borderwidth) * 2)}
-	//	right := rl.Rectangle{X: screen.E - float32(borderwidth), Y: screen.N + float32(borderwidth), Width: float32(borderwidth), Height: screen.S - (float32(borderwidth) * 2)}
-	//	topLeft := rl.Rectangle{X: screen.N, Y: screen.N, Width: float32(borderwidth), Height: float32(borderwidth)}
-	//	topRight := rl.Rectangle{X: screen.E - float32(borderwidth), Y: screen.N, Width: float32(borderwidth), Height: float32(borderwidth)}
-	//	bottomLeft := rl.Rectangle{X: screen.W, Y: screen.S - float32(borderwidth), Width: float32(borderwidth), Height: float32(borderwidth)}
-	//	bottomRight := rl.Rectangle{X: screen.E - float32(borderwidth), Y: screen.S - float32(borderwidth), Width: float32(borderwidth), Height: float32(borderwidth)}
+	top := rl.Rectangle{X: screen.W + float32(borderwidth), Y: screen.N, Width: screen.E - (float32(borderwidth) * 2), Height: float32(borderwidth)}
+	bottom := rl.Rectangle{X: screen.W + float32(borderwidth), Y: screen.S - float32(borderwidth), Width: screen.E - (float32(borderwidth) * 2), Height: float32(borderwidth)}
+	left := rl.Rectangle{X: screen.W, Y: screen.N + float32(borderwidth), Width: float32(borderwidth), Height: screen.S - (float32(borderwidth) * 2)}
+	right := rl.Rectangle{X: screen.E - float32(borderwidth), Y: screen.N + float32(borderwidth), Width: float32(borderwidth), Height: screen.S - (float32(borderwidth) * 2)}
+	topLeft := rl.Rectangle{X: screen.N, Y: screen.N, Width: float32(borderwidth), Height: float32(borderwidth)}
+	topRight := rl.Rectangle{X: screen.E - float32(borderwidth), Y: screen.N, Width: float32(borderwidth), Height: float32(borderwidth)}
+	bottomLeft := rl.Rectangle{X: screen.W, Y: screen.S - float32(borderwidth), Width: float32(borderwidth), Height: float32(borderwidth)}
+	bottomRight := rl.Rectangle{X: screen.E - float32(borderwidth), Y: screen.S - float32(borderwidth), Width: float32(borderwidth), Height: float32(borderwidth)}
 
 	rl.SetTargetFPS(60)
 
-	var inputText string
-
 	for !rl.WindowShouldClose() {
 
-		//rl.UpdateCamera(&camera)
+		rl.UpdateCamera(&camera)
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
+		// Debug Borders
 		//rl.DrawRectangleRec(top, rl.Yellow)
 		//rl.DrawRectangleRec(left, rl.Green)
 		//rl.DrawRectangleRec(right, rl.Blue)
@@ -92,25 +83,14 @@ func main() {
 		//rl.DrawRectangleRec(bottomLeft, rl.Red)
 		//rl.DrawRectangleRec(bottomRight, rl.Red)
 
-		//rl.BeginMode3D(camera)
-		//rl.DrawGrid(50, 1.0)
-		//rl.DrawModel(rl.LoadModelFromMesh(rl.GenMeshPoly(6, 10)), rl.NewVector3(float32(screenPoint.X), float32(screenPoint.Y), 0), 1, rl.Green)
-		//	rl.EndMode3D()
+		rl.BeginMode3D(camera)
+		rl.DrawGrid(50, 1.0)
+		rl.DrawModel(rl.LoadModelFromMesh(rl.GenMeshPoly(6, 10)), rl.NewVector3(float32(screenPoint.X), float32(screenPoint.Y), 0), .25, rl.Green)
+		rl.EndMode3D()
 
-		secondCamera.Zoom += float32(rl.GetMouseWheelMove()) * 0.05
-
-		if secondCamera.Zoom > 3.0 {
-			secondCamera.Zoom = 3.0
-		} else if secondCamera.Zoom < 0.1 {
-			secondCamera.Zoom = 0.1
-		}
-		raygui.Label(rl.NewRectangle(500, 330, 200, 20), "TextBox")
-		inputText = raygui.TextBox(rl.NewRectangle(500, 350, 200, 20), inputText) // Debug Text
-		rl.BeginMode2D(secondCamera)
-		rl.DrawPoly(rl.NewVector2(float32(screenPoint.X), float32(screenPoint.Y)), 6, 12, 0, rl.Red)
 		rl.DrawText("Camera X: "+fmt.Sprintf("%.2f", camera.Position.X), 10, 10, 20, rl.Gray)
 		rl.DrawText("Camera Y: "+fmt.Sprintf("%.2f", camera.Position.Y), 10, 30, 20, rl.Gray)
-		rl.DrawText("Camera Z: "+fmt.Sprintf("%.2f", secondCamera.Zoom), 10, 50, 20, rl.Gray)
+		rl.DrawText("Camera Z: "+fmt.Sprintf("%.2f", camera.Position.Z), 10, 50, 20, rl.Gray)
 		rl.DrawText("Mouse X: "+fmt.Sprintf("%d", rl.GetMouseX()), 10, 70, 20, rl.Gray)
 		rl.DrawText("Mouse Y: "+fmt.Sprintf("%d", rl.GetMouseY()), 10, 90, 20, rl.Gray)
 		rl.DrawText("FPS: "+fmt.Sprintf("%.2f", rl.GetFPS()), int32(screen.E)-5-rl.MeasureText("FPS: "+fmt.Sprintf("%.2f", rl.GetFPS()), 20), 10, 20, rl.Gray)
@@ -123,46 +103,46 @@ func main() {
 		} else if rl.GetMouseX()-cursor.intrad <= 0 {
 			rl.SetMousePosition(0+int(cursor.intrad), int(rl.GetMouseY()))
 		}
-		//
-		//		if rl.GetMouseY()+cursor.intrad > int32(rl.GetScreenHeight()) {
-		//			rl.SetMousePosition(int(rl.GetMouseX()), rl.GetScreenHeight()-int(cursor.intrad))
-		//		} else if rl.GetMouseY()-cursor.intrad < 0 {
-		//			rl.SetMousePosition(int(rl.GetMouseX()), 0+int(cursor.intrad))
-		//		}
-		//
-		//		if rl.CheckCollisionPointRec(rl.GetMousePosition(), right) {
-		//			camera.Position.X += panSpeed
-		//			camera.Target.X += panSpeed
-		//		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), left) {
-		//			camera.Position.X -= panSpeed
-		//			camera.Target.X -= panSpeed
-		//		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), bottom) {
-		//			camera.Position.Z += panSpeed
-		//			camera.Target.Z += panSpeed
-		//		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), top) {
-		//			camera.Position.Z -= panSpeed
-		//			camera.Target.Z -= panSpeed
-		//		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), bottomRight) {
-		//			camera.Position.X += panSpeed
-		//			camera.Target.X += panSpeed
-		//			camera.Position.Z += panSpeed
-		//			camera.Target.Z += panSpeed
-		//		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), topLeft) {
-		//			camera.Position.X -= panSpeed
-		//			camera.Target.X -= panSpeed
-		//			camera.Position.Z -= panSpeed
-		//			camera.Target.Z -= panSpeed
-		//		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), topRight) {
-		//			camera.Position.X += panSpeed
-		//			camera.Target.X += panSpeed
-		//			camera.Position.Z -= panSpeed
-		//			camera.Target.Z -= panSpeed
-		//		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), bottomLeft) {
-		//			camera.Position.X -= panSpeed
-		//			camera.Target.X -= panSpeed
-		//			camera.Position.Z += panSpeed
-		//			camera.Target.Z += panSpeed
-		//		}
+
+		if rl.GetMouseY()+cursor.intrad > int32(rl.GetScreenHeight()) {
+			rl.SetMousePosition(int(rl.GetMouseX()), rl.GetScreenHeight()-int(cursor.intrad))
+		} else if rl.GetMouseY()-cursor.intrad < 0 {
+			rl.SetMousePosition(int(rl.GetMouseX()), 0+int(cursor.intrad))
+		}
+
+		if rl.CheckCollisionPointRec(rl.GetMousePosition(), right) {
+			camera.Position.X += panSpeed
+			camera.Target.X += panSpeed
+		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), left) {
+			camera.Position.X -= panSpeed
+			camera.Target.X -= panSpeed
+		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), bottom) {
+			camera.Position.Z += panSpeed
+			camera.Target.Z += panSpeed
+		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), top) {
+			camera.Position.Z -= panSpeed
+			camera.Target.Z -= panSpeed
+		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), bottomRight) {
+			camera.Position.X += panSpeed
+			camera.Target.X += panSpeed
+			camera.Position.Z += panSpeed
+			camera.Target.Z += panSpeed
+		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), topLeft) {
+			camera.Position.X -= panSpeed
+			camera.Target.X -= panSpeed
+			camera.Position.Z -= panSpeed
+			camera.Target.Z -= panSpeed
+		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), topRight) {
+			camera.Position.X += panSpeed
+			camera.Target.X += panSpeed
+			camera.Position.Z -= panSpeed
+			camera.Target.Z -= panSpeed
+		} else if rl.CheckCollisionPointRec(rl.GetMousePosition(), bottomLeft) {
+			camera.Position.X -= panSpeed
+			camera.Target.X -= panSpeed
+			camera.Position.Z += panSpeed
+			camera.Target.Z += panSpeed
+		}
 	}
 
 	rl.CloseWindow()
