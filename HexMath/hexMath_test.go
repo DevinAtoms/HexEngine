20f6876dec2805bc7d5cca4c978bae0110b4c54d
Devin Adams <68407562+insultfromafar@users.noreply.github.com>
2022-02-28 14:31:37 -0800

    Module Split

--- HexMath/hexMath_test.go
+++ HexMath/hexMath_test.go
@@ -1,4 +1,4 @@
-package HexEngine
+package HexMath
 
 import (
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -8,8 +8,8 @@ import (
 // I'm trying to learn better testing habits so here's an attempt to do some testing on my hexMath
 
 func TestCoords(t *testing.T) {
-	coords := HexMath_Go.NewCoords(1, -1, 0)
-	hex, err := HexMath_Go.GetHexCoord(coords)
+	coords := NewCoords(1, -1, 0)
+	hex, err := GetHexCoord(coords)
 	if hex == rl.Vector3Zero() || err == nil {
 		t.Fatalf("%v", err)
 	}
